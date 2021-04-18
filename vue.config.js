0db0abd02f8be6a4dab57d8325747cf2167d3f9b
Arne Wendt <arne.wendt@tuhh.de>
2021-04-18 18:08:36 +0200

    use original source files for use with node and bundler (webpack, etc.)

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,27 @@
 const path = require('path');
 
+const ext_factory = (root, pkg) => {
+  return {
+    root: root,
+    commonjs: pkg,
+    commonjs2: pkg,
+    amd: pkg
+  };
+};
+
 module.exports = (() => {
   // building library target
   if (process.env.VUE_BUILD_TARGET === 'lib') {
     return {
+      outputDir: path.join(__dirname, './bundle'),
       chainWebpack: config => {
-        // do not bundle vuetify (necessary?)
+        // do not bundle vuetify, vuex and vue (should be excluded per default)
         // we are not transpiling vuetify dependencies as well
         config
           .externals({
-            vuetify: 'vuetify'
+            vuetify: ext_factory('Vuetify', 'vuetify'),
+            vuex: ext_factory('Vuex', 'vuex'),
+            vue: ext_factory('Vue', 'vue')
           })
       }
 
